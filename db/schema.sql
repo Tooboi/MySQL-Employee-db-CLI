49171a44995709d95a7c8e70391828915fb963aa
Tooboi <japica123@gmail.com>
2022-12-13 15:48:42 -0500

    database files frameworked

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,19 @@ CREATE DATABASE factory_db;
 USE factory_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
-)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary INT
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,21 @@
-INSERT INTO factory_db (id, name)
-VALUES (1, "Nursing"),
-        (2, "Engineering"),
-        (3, "Janatorial"),
-        (4, "Law");
+INSERT INTO department (name)
+VALUES ("Nursing"),
+        ("Engineering"),
+        ("Janatorial"),
+        ("Legal");
+
+INSERT INTO role (title, salary)
+VALUES ("Head Nurse", 80000),
+        ("Doctor", 180000),
+        ("Cleaner", 65000),
+        ("Maintnence", 90000),
+        ("Lawyer", 200000),
+        ("Data Analytics", 100000);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Rose", "Flanders", 1),
+        ("Gary", "Pimmento", 1),
+        ("Lisa", "Bran", 3),
+        ("Florence", "Garcia", 4),
+        ("Barry", "Lawburger", 5),
+        ("Ben", "Sanders", 6);
