abd807f9a3abcfbd983725872a788d7351e21606
Tooboi <japica123@gmail.com>
2022-12-16 15:28:21 -0500

    schema update

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,16 +5,16 @@ VALUES ("Nursing"),
         ("Legal");
 
 INSERT INTO role (title, salary, department)
-VALUES ("Head Nurse", 80000),
-        ("Doctor", 180000),
-        ("Cleaner", 65000),
-        ("Maintnence", 90000),
-        ("Lawyer", 200000),
-        ("Data Analytics", 100000);
+VALUES ("Head Nurse", 80000, 1),
+        ("Doctor", 180000, 1),
+        ("Cleaner", 65000, 3),
+        ("Maintnence", 90000, 2),
+        ("Lawyer", 200000, 4),
+        ("Data Analytics", 100000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id)
 VALUES ("Rose", "Flanders", 1),
-        ("Gary", "Pimmento", 1),
+        ("Gary", "Pimmento", 2),
         ("Lisa", "Bran", 3),
         ("Florence", "Garcia", 4),
         ("Barry", "Lawburger", 5),
